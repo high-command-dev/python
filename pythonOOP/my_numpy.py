330d8df01d93f4f3353c1ed58d9ed014df96ad2c
high-command-dev <ssbruh200@gmail.com>
2024-12-30 00:14:30 +0530

    that all for today battery low

--- pythonOOP/my_numpy.py
+++ pythonOOP/my_numpy.py
@@ -30,6 +30,58 @@ import numpy as np
 # print(arr.ndim)
 
 "5D array"
-arr=np.array([[[[[1,2,3]]]]])
-print(arr)
-print(arr.ndim)
+# arr=np.array([[[[[1,2,3]]]]])
+# print(arr)
+# print(arr.ndim)
+
+"from range"
+# arr = np.arange(11,20)
+# print(arr)
+
+"from range and reshaping"
+# arr = np.arange(1,7).reshape((3,2))
+# print(arr)
+
+" making a sparse matrix"
+# arr = np.zeros((4,2),dtype=int)
+# print(arr)
+
+"opposite of sparse"
+# arr = np.ones((4,2),dtype=int)
+# print(arr)
+
+
+"Various Attributes of numpy Array"
+"""
+(i) ndim
+(ii) shape
+(iii) size
+(iv) dtype
+(v) itemsize
+"""
+"for integer"
+# list1 = [1,2,3,4,5]
+# arr = np.array(list1)
+# print(arr) 
+# print(arr.ndim)
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+# print(arr.itemsize)
+
+"for float"
+
+# list1 = [1.,2.,3.,4.,5.]
+# arr = np.array(list1)
+# print(arr) 
+# print(arr.ndim)
+# print(arr.shape)
+# print(arr.size)
+# print(arr.dtype)
+# print(arr.itemsize)
+
+"Indexing"
+arr = np.array([1,2,3,4,5])
+print(arr[0]) # 1
+print(arr[-1]) # 5
+
