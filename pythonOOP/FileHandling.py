fcde280e306e96494c7618a1f1592a46c3b1e229
high-command-dev <ssbruh200@gmail.com>
2024-12-31 00:15:25 +0530

    modified

--- pythonOOP/FileHandling.py
+++ pythonOOP/FileHandling.py
@@ -3,6 +3,7 @@ File is used to store data permanently in Memory
 
 Operations on File:
 1) Open a File
+
 f1 = open('abc.txt')
 File Modes (r,w,x,a,t,b,+)
 1) 'r' means open a file in read mode
@@ -12,53 +13,42 @@ File Modes (r,w,x,a,t,b,+)
 5)'b' ->binary
 6)'+' ->open a file for updating
 
-
-
-
 2) Read/Write
 
-
-
-
-
 3) Close the File
-
-
-
-
 """
 
-# f = open('new.txt')
-# print(f.read())
+f = open('new.txt')
+print(f.read())
 
 f = open('new.txt','w')
 f.write("""Hello This is Ashutosh
 This is the File for Python File Handling\nPython is impotant for interviews\n""")
 f.close()
 
-# f = open('new.txt')
-# print(f.read())
+f = open('new.txt')
+print(f.read())
 
 f = open('new.txt','a')
 f.write("So Practice it daily")
 f.close()
 
-# f = open('new.txt')
-# print(f.read())
+f = open('new.txt')
+print(f.read())
 
 f = open('new.txt')
-# print(f.read(5))
-# print(f.tell())
-# f.seek(10)
-# print(f.tell())
-# print(f.read(11))
+print(f.read(5))
+print(f.tell())
+f.seek(10)
+print(f.tell())
+print(f.read(11))
 
-# f.seek(0)
-# for line in f:
-#   print(line)
+f.seek(0)
+for line in f:
+  print(line)
 
-# print(f.readline())
-# print(f.readline(20))
-# print(f.readline(200)) ->it will only read a single line
+print(f.readline())
+print(f.readline(20))
+print(f.readline(200)) # ->it will only read a single line
 
-# print(f.readlines()) #provide output in a from of list
+print(f.readlines()) #provide output in a from of list
